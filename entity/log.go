8b6541bbb1eea419b9d8b40c3e9c94f9625d97bf
khalidzahra <khalidzahra923@gmail.com>
2023-04-29 07:10:18 +0300

    Update log entity format to match information received from robots

--- entity/log.go
+++ entity/log.go
@@ -2,6 +2,9 @@ package entity
 
 type Log struct {
 	ID        uint64 `json:"id" gorm:"primary_key;auto_increment"`
+	LogType   string `json:"logType"`
+	Name      string `json:"name"`
+	Status    string `json:"status"`
 	Timestamp string `json:"timestamp"`
 	Message   string `json:"message"`
 	RobotID   uint64 `json:"robotId"`
